b8ea7f2a4f876c117257c57d12539d2307fd7dbf
We1rdC0der <landinsphone@gmail.com>
2025-07-26 20:28:03 -0400

    Add files via upload

---
+++ content/itemdata.js
@@ -0,0 +1,24 @@
+window.WISHLIST_ITEMS = [
+  {
+    name: "Cool Headphones",
+    amount: 49.99,
+    bio: "Wireless, noise-cancelling headphones.",
+    longBio: "These headphones offer 30 hours of battery life and amazing sound quality.",
+    images: [
+  "assets/images/other/placeholder.jpeg"
+],
+imgUrl: "assets/images/other/placeholder.jpeg",
+    link: "https://www.youtube.com"
+  },
+  {
+    name: "Test Item",
+    amount: 19.99,
+    bio: "Test bio.",
+    longBio: "Test long bio.",
+    imgUrl: "assets/images/other/placeholder.jpeg",
+    images: ["assets/images/other/placeholder.jpeg",
+      "assets/images/other/placeholder.jpeg", "assets/images/other/placeholder.jpeg"
+    ],
+    link: "https://www.youtube.com"
+  }
+];
